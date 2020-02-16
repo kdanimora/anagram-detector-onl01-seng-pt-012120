aebd318ab408f406ca170dcae6972d6d7582eda4
Karen Daniela <kdaniimora@gmail.com>
2020-02-16 22:56:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,18 @@
 # Your code goes here!
-def Anagram 
-  attr_accessor :word
- 
- def initialize(word)
- @word = word 
- end 
- 
 
-def match(array)
-   array.find_all {|e| e.split('').sort == word.split('').sort}
- end 
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  
+  def match(array) 
+    array.find_all {|e| e.split('').sort == word.split('').sort}
+  end
+  
 end 
+
 
 
